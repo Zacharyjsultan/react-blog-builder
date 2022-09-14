4262c1431a73fbd0e883e83e39634a7995bdc7f1
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-14 14:33:35 -0700

    home.js useState

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -6,14 +6,18 @@ import Editor from '../Editor/Editor';
 import './Home.css';
 
 export default function Home() {
-  // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubTitle] = useState('');
+  const [font, setFont] = useState('Roboto');
+  const [align, setAlign] = useState('left');
+  const [text, setText] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
       <Preview />
       <Editor />
-      <div></div>
+      
     </main>
   );
 }
