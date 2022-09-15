44186475dfa1c0ba6109a1bfc5af12a8b8cc8c82
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-14 17:14:07 -0700

    editor.js

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Preview.css';
 
 export default function Preview({ title, subtitle, font, align, text }) {
-  // implement a Preview screen here
+  
   return (
     <div className={`preview ${font}`} style={{ textAlign: align }}>
       <h1>{title}</h1>
