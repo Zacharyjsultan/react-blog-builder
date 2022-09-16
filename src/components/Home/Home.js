9e591e5422b7d43de7364e28bc4fab085614cc95
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-16 15:56:26 -0700

    deleting state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import Editor from '../Editor/Editor';
 import './Home.css';
 
 export default function Home() {
-  const [title, setTitle] = useState('');
+  
   const [subtitle, setSubTitle] = useState('');
   const [font, setFont] = useState('Roboto');
   const [align, setAlign] = useState('left');
@@ -15,8 +15,8 @@ export default function Home() {
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} text={text} font={font} align={align}/>
-      <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubTitle={setSubTitle} font={font} setFont={setFont} align={align} setAlign={setAlign} text={text} setText={setText} />
+      <Preview title={} subtitle={subtitle} text={text} font={font} align={align}/>
+      <Editor title={} setTitle={setTitle} subtitle={subtitle} setSubTitle={setSubTitle} font={font} setFont={setFont} align={align} setAlign={setAlign} text={text} setText={setText} />
       
     </main>
   );
